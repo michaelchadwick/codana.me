b7a328bc3830b08fe7a194742d6fec0c1d331507
Michael Chadwick <michael.chadwick@gmail.com>
2017-04-27 00:32:30 -0400

    added nifty overlay-on-search-focus effect

--- assets/scripts/all.js
+++ assets/scripts/all.js
@@ -1,9 +1,23 @@
-SimpleJekyllSearch.init({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  dataSource: '/search.json',
-  searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
-  noResultsText: 'No results found',
-  limit: 5,
-  fuzzy: false,
-})
+$(function() {
+  SimpleJekyllSearch.init({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    dataSource: '/search.json',
+    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+    noResultsText: 'No results found',
+    limit: 5,
+    fuzzy: false,
+  });
+
+  $('#search-input').on('focus', function() {
+    $(this).addClass('selected');
+    $(this).parent().addClass('selected');
+    $('.overlay').addClass('enabled');
+  });
+  $('#search-input').on('blur', function() {
+    $(this).removeClass('selected');
+    $(this).parent().removeClass('selected');
+    $('.overlay').removeClass('enabled');
+  });
+
+});
